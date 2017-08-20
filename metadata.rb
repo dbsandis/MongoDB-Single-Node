42b482e7fa1cc6393919ee406465be8e9ecc313a
dbsandis <dbsandis@gmail.com>
2017-08-20 14:53:30 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,7 @@
-site :opscode
+name        "mongodb-mysite"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "MyOrg"
+license     "Apache 2.0"
+version     "1.0.0"
 
-cookbook 'mongodb'
+depends 'mongodb'
